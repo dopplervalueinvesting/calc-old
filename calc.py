9a70f45106acd58847548336c656f11f4acd055f
Jason Hsu <finance@jasonhsu.com>
2012-01-27 23:45:42 -0600

    Working on lineitem_codes

--- calc.py
+++ calc.py
@@ -11,10 +11,34 @@ dir_input = dir_doppler + '/input'
 dir_output = dir_doppler + '/output'
 os.chdir(dir_analysis)
 
+# Input: 2-D data array
+# Output: 1-D data array representing the (n_local+1) -th column of the input array
 def column (array_local, n_local):
     array_transpose = zip (*array_local)
     return array_transpose [n_local]
 
+# Input: 2D data array
+# Output: 1D data array representing the (n_local+1) -th column of the input array, minus the first entry
+def column_data (array_local, n_local):
+    array1 = column (array_local, n_local)
+    array2 = array1 [1:]
+    return array2
+
+# Cuts off the first two entries of an array and then reverses it
+# Used to process the financial numbers in the stock data
+# The first column is the line item.  The second column is the code.
+# The numbers to process are in the rest of the data.
+# Input: 2-D data array
+# Output: 1-D data array representing the (n_local+1) -th row of the input array
+def row_rev (array_local, n_local):
+    array1 = array_local [n_local] # (n_local+1) -th row
+    array2 = array1 [2:]
+    array3 = array2 [::-1] # reverses array
+    return array3
+
+# This defines the class CSVfile (filename).
+# Input: name of csv file
+# Output: 2-D array fed by the input file
 class CSVfile:
     def __init__ (self, filename):
         self.filename = filename
@@ -27,36 +51,78 @@ class CSVfile:
                 localarray.append (row)
         return localarray
 
+# This defines the class Stock (symbol)
+# Input: stock symbol
+# Outputs: 2-D data array fed by the input file and 1-D data arrays
 class Stock:
     def __init__ (self, symbol):
         self.symbol = symbol
 
-    def stockdata (self)
-        file_stock = CSVfile (dir_input + '/' + stock_symbol + '.csv')
+    # Output: 2-D data array
+    def data (self):
+        file_stock = CSVfile (dir_input + '/' + self.symbol + '.csv')
         array_stock = file_stock.filearray ()
         return array_stock
 
+    # Output: string
+    def name (self):
+        file_companies = CSVfile (dir_input + '/company_list.csv')
+        array_companies = file_companies.filearray ()
+        list_symbols = column_data (array_companies, 0)
+        list_names = column_data (array_companies, 1)
+        i_symbol = list_symbols.index(self.symbol)
+        return list_names [i_symbol]
+
+    # Output: 1-D array of integers (top row, excluding the first two columns)
+    def years (self):
+        array1 = self.data ()
+        array2 = row_rev (array1, 0)
+        return array2
+
+    # Output: 1-D array of integers (first column, excluding the top row)
+    def lineitem_titles (self):
+        array1 = self.data ()
+        array2 = column_data (array1, 0)
+        return array2
+
+    # Output: 2D array of integers
+    # First column: specific codes
+    # Second column: general codes
+    # Third column: signs, +1 or -1
+    def lineitem_codes (self):
+        array1 = self.data ()
+        spec_local = column_data (array1, 1) # First column of stock data
+        finalarray = []
+        file_codes = CSVfile (dir_analysis + '/codes.csv')
+        array_codefile = file_codes.filearray ()
+        spec_codefile = column_data (array_codefile, 1)
+        gen_codefile = column_data (array_codefile, 3)
+        signs_codefile = column_data (array_codefile, 2)
+        r = 0
+        for item in spec_local: # Go through the specific codes in the stock data
+            i_spec = spec_codefile.index (item)
+            gen_local = gen_codefile [i_spec]
+            signs_local = signs_codefile [i_spec]
+            finalarray [r][0] = item
+            finalarray [r][1] = signs_local
+            finalarray [r][2] = gen_local
+            r = r + 1
+        return finalarray
+
+
         
 
-file_codes = CSVfile (dir_analysis + '/codes.csv')
-array_codes = file_codes.filearray ()
-cat_spec = column (array_codes, 1)[1:]
-sign = column (array_codes, 2)[1:]
-cat_gen = column (array_codes, 3)[1:]
 
-file_companies = CSVfile (dir_input + '/company_list.csv')
-array_companies = file_companies.filearray ()
-list_symbols = column (array_companies, 0) [1:]
-list_names = column (array_companies, 1) [1:]
 
-stock_symbol = raw_input ('Enter the stock symbol of the company to analyze:\n')
-i_symbol = list_symbols.index(stock_symbol)
-stock_name = list_names [i_symbol]
+# stock_symbol = raw_input ('Enter the stock symbol of the company to analyze:\n')
+stock_symbol = 'fast'
+mystock = Stock (stock_symbol)
+# print mystock.data()
+print mystock.name()
+print mystock.years()
+# myyears= mystock.years()
+# print myyears [0]
+print mystock.lineitem_titles()
+print mystock.lineitem_codes()
 
-# file_stock = CSVfile (dir_input + '/' + stock_symbol + '.csv')
-# array_stock = file_stock.filearray ()
-print array_stock
 
-# if stock_symbol in list_symbols
-#     imatch = 
-#    stock_name = 
